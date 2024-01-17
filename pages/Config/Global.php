2e4cea6b0c83f5d93a9d3a41a852bf8ca682108f
Skeletus <Samol_88@hotmail.com>
2024-01-17 00:43:47 -0500

    deleting files

--- pages/Config/Global.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define("Server", "localhost");
-    define("Database", "bd_sisventas");
-    define("Username", "root");
-    define("Password","");
-    define("DataSourceName", "mysql:host=".Server.";dbname=".Database);
-?>
