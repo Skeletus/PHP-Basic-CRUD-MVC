1dd9aed2e91ba74b617f247b32ee1efb4ffdd281
Skeletus <Samol_88@hotmail.com>
2024-01-18 02:13:44 -0500

    Register Create and Update Categoria

--- Controller/CategoriaController.php
+++ Controller/CategoriaController.php
@@ -18,7 +18,7 @@ switch ($_REQUEST["Operator"])
                     <i class="icon-gear"></i>
                     </button>
                     <div class="dropdown-menu">
-                    <a class="dropdown-item"><i class="icon-edit"></i> Editar</a>
+                    <a class="dropdown-item" data-toggle="modal" data-target="#update_categoria" onclick="ObtenerCategoriaPorID('.$datos[$i]['categoria_id'].');"><i class="icon-edit"></i> Editar</a>
                     <a class="dropdown-item"><i class="icon-trash"></i> Eliminar</a>
                     </div>
                     </div>',
@@ -37,6 +37,61 @@ switch ($_REQUEST["Operator"])
             );
         }
         echo json_encode($resultados);
+    break;
+
+    case "registrar_categoria":
+        if(isset($_POST["nombre"], $_POST["descripcion"])
+         && !empty($_POST["nombre"]) && !empty($_POST["descripcion"]))
+        {
+            $nombre = $_POST["nombre"];
+            $descripcion = $_POST["descripcion"];
+            if($categoria->RegistrarCategoria($nombre, $descripcion))
+            {
+                $response = "success";
+            }
+            else
+            {
+                $response = "requiere id";
+            }
+        }
+        echo $response;
+    break;
+
+    case "obtener_categoria_por_id":
+        if (isset($_POST["categoria_id"]) && !empty($_POST["categoria_id"]))
+        {
+            $data = $categoria->ObtenerCategoriaPorID($_POST["categoria_id"]);
+            if($data)
+            {
+                $list[] = array(
+                    "id"=>$data['categoria_id'],
+                    "nombre"=>$data['nombre'],
+                    "descripcion"=>$data['descripcion']
+                );
+                echo json_encode($list);
+            }
+        }
+
+    break;
+
+    case "actualizar_categoria":
+        if(isset($_POST["nombre"], $_POST["descripcion"], $_POST["categoria_id"])
+         && !empty($_POST["nombre"]) && !empty($_POST["descripcion"]) && !empty($_POST["categoria_id"]))
+        {
+            $categoria_id = $_POST["categoria_id"];
+            $nombre = $_POST["nombre"];
+            $descripcion = $_POST["descripcion"];
+            if($categoria->ActualizarCategoria($categoria_id, $nombre, $descripcion))
+            {
+                $response = "success";
+            }
+            else
+            {
+                $response = "requiere id";
+            }
+        }
+        echo $response;
+
     break;
 }
 
