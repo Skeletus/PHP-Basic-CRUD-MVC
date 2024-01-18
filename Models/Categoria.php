b91e2ad8fbb445bc6c5e02e3eb8a90e031e27a55
Skeletus <Samol_88@hotmail.com>
2024-01-18 02:13:16 -0500

    Register Get and Update in Model Categoria

--- Models/Categoria.php
+++ Models/Categoria.php
@@ -31,6 +31,56 @@ class Categoria
         }
         return false;
     }
+
+    /// <summary>
+    /// Function to create a new Categoria
+    /// </summary>
+    function RegistrarCategoria($nombre, $descripcion)
+    {
+        $query = "INSERT INTO categoria(nombre, descripcion) VALUES (?, ?)";
+        $result = $this->con->prepare($query);
+        $result->bindParam(1,$nombre);
+        $result->bindParam(2,$descripcion);
+        if($result->execute())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Function to get Categoria by ID
+    /// </summary>
+    function ObtenerCategoriaPorID($categoria_id)
+    {
+        $query = "SELECT * FROM categoria WHERE categoria_id = ?";
+        $result = $this->con->prepare($query);
+        $result->bindParam(1,$categoria_id);
+        if($result->execute())
+        {
+            return $result->fetch(PDO::FETCH_ASSOC);
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Function to get Categoria by ID
+    /// </summary>
+    function ActualizarCategoria($categoria_id, $nombre, $descripcion)
+    {
+        $query = "UPDATE categoria SET nombre=?, descripcion=? WHERE categoria_id = ?";
+        $result = $this->con->prepare($query);
+        $result->bindParam(1,$nombre);
+        $result->bindParam(2,$descripcion);
+        $result->bindParam(3,$categoria_id);
+        if($result->execute())
+        {
+            return true;
+        }
+        return false;
+
+    }
+
 }
 
 ?>
